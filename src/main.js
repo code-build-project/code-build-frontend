cc3779b2544c38d13fcbb1a9c5f4e4b61233dc3e
CodeBuild <code-build@yandex.ru>
2021-06-21 23:09:09 +0400

    reg

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import './directives/clickOutside';
 
 // Добавление токена в заголовок axios и адрес сервера
 const token = localStorage.token || '';
-axios.defaults.baseURL = 'https://fast-ocean-40880.herokuapp.com';
+// axios.defaults.baseURL = 'https://fast-ocean-40880.herokuapp.com';
+axios.defaults.baseURL = 'http://127.0.1.1:5000/';
 axios.defaults.headers.common['Authorization'] = token.substring(7);
 
 // Доавление axios в прототипы, чтобы не импортировать в каждый файл
