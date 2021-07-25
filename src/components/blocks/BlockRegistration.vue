49994a612f10334b1568518916a8db8e11fb8240
CodeBuild <code-build@yandex.ru>
2021-07-25 19:50:49 +0400

    block reg

--- src/components/blocks/BlockRegistration.vue
+++ src/components/blocks/BlockRegistration.vue
@@ -1,86 +1,77 @@
 <template>
   <div class="reg__wrap">
     <div class="reg">
-      <div class="reg__info">
+      <h1 class="reg__title">
         Зарегистрируйся в codebuild и получи бесплатный доступ к более чем 20 курсам
-      </div>
+      </h1>
 
       <div class="reg__input">
-        <div class="reg__input-legend">
-          Email
-        </div>
+        <div class="reg__input-legend">Email</div>
 
-        <input
-          class="reg__input-field"
-          value=""
-          placeholder="your.email@example.com"
-        />
+        <input class="reg__input-field" value="" placeholder="your.email@example.com" />
 
-        <div class="reg__input-icon">
-          <icon-arrow />
-        </div>
+        <v-icon class="reg__icon-arrow" path="img/arrow.svg" width="23px" height="23px" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import IconArrow from '@/icons/IconArrow.vue'
+import VIcon from '@/components/common/VIcon.vue';
 
 export default {
   name: 'BlockRegistration',
-  components: { IconArrow }
-}
+  components: { VIcon }
+};
 </script>
 
 <style lang="scss" scoped>
 .reg__wrap {
-  @extend .cb_center;
+  @extend .flex_row-center-center;
   width: 100%;
-  background: #256cfe;
+  background: $color-blue;
 }
 
 .reg {
-  @extend .cb_row-between;
+  @extend .flex_row-center-between;
   width: 1160px;
   height: 364px;
 
-  &__info {
+  &__title {
     width: 380px;
 
     font-family: 'Circe';
     font-size: 30px;
     line-height: 39px;
     letter-spacing: -0.025em;
-    color: #ffffff;
+    color: $color-white;
   }
 
   &__input {
     position: relative;
     width: 496px;
-    height: 80px;
+    height: 76px;
+    margin-top: 20px;
 
     font-family: 'Circe';
-    border-bottom: 1px solid #ffffff;
+    border-bottom: 1px solid $color-white;
   }
 
   &__input-legend {
     font-family: 'Circe';
     font-size: 17px;
-    line-height: 19px;
-    color: #ffffff;
+    color: $color-white;
   }
 
   &__input-field {
-    margin-top: 20px;
+    margin-top: 15px;
 
     font-size: 26px;
-    line-height: 19px;
     letter-spacing: -0.02em;
 
     border: none;
     background: transparent;
-    color: #ffffff;
+    color: $color-white;
 
     outline: none;
 
@@ -88,9 +79,12 @@ export default {
       color: #518aff;
     }
   }
+}
 
-  &__input-icon {
-    @extend .cb_center;
+// icons
+.reg__icon {
+  &-arrow {
+    @extend .flex_row-center-center;
     position: absolute;
     width: 50px;
     height: 50px;
@@ -100,18 +94,16 @@ export default {
 
     border: 2px solid #3a7afe;
     border-radius: 10px;
+    stroke: $color-white;
   }
 }
 
 // hovers
 :hover.reg {
-  &__input-icon {
-    background: #ffffff;
+  &__icon-arrow {
     cursor: pointer;
-
-    ::v-deep path {
-      stroke: #256cfe;
-    }
+    stroke: $color-blue;
+    background: $color-white;
   }
 }
 </style>
