290a534fe5438ba0c1996ba1d0e46470fb0d5d7c
Shamil <shamil.tepsurkaev@simbirsoft.com>
2022-08-07 14:04:49 +0400

    scroll stop for safari

--- src/mixins/popup.js
+++ src/mixins/popup.js
@@ -1,9 +1,40 @@
 export default {
+    methods: {
+        stopScroll() {
+            if (!document.body.hasAttribute('data-body-scroll-fix')) {
+                let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+          
+                document.body.setAttribute('data-body-scroll-fix', scrollPosition);
+                document.body.style.overflow = 'hidden';
+                document.body.style.position = 'fixed';
+                document.body.style.top = '-' + scrollPosition + 'px';
+                document.body.style.left = '0';
+                document.body.style.width = '100%';
+            }
+        },
+
+        startScroll() {
+            if (document.body.hasAttribute('data-body-scroll-fix')) {
+                let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+            
+                document.body.removeAttribute('data-body-scroll-fix');
+            
+                document.body.style.overflow = '';
+                document.body.style.position = '';
+                document.body.style.top = '';
+                document.body.style.left = '';
+                document.body.style.width = '';
+            
+                window.scroll(0, scrollPosition);
+            }
+        }
+    },
+    
     mounted() {
-        document.body.style.overflow = "hidden";
+        this.stopScroll();
     },
 
     beforeDestroy() {
-        document.body.style.overflow = "";
+        this.startScroll();
     }
 };
